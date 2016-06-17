1d56937695f718b61a3b3c50347e3e87e7a61ce6
plx <784580872@qq.com>
2016-06-17 08:33:36 +0800

    CylicBarrier原理分析

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Java-Concurrency-Practise
+> 个人并发编程练习以及总结
+
+### 同步工具类
+
+#### 1.CylicBarrier
+Barrier为"栅栏"的意思,在此表示阻塞线程继续执行，让线程进入到等待状态(TIMED_WAITING)。
+当所有线程都到达"栅栏"点时，栅栏就会倒下(tripped),此时会由最后一个线程执行"栅栏"中的Barrier Action,
+在执行之后,所有的线程就能继续往下执行。
+
--- src/main/java/com/panlingxiao/syn/aid/CyclicBarrierTest.java
+++ src/main/java/com/panlingxiao/syn/aid/CyclicBarrierTest.java
@@ -1,7 +1,100 @@
 package com.panlingxiao.syn.aid;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 /**
  * Created by panlingxiao on 2016/6/16.
  */
 public class CyclicBarrierTest {
+
+    static boolean isDone = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        final float[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        Solver solver = new Solver(matrix);
+    }
+
+
+    static boolean done() {
+        return isDone;
+    }
+
+    public static class Solver {
+        int N;
+        float[][] matrix;
+        final CyclicBarrier barrier;
+        final List<Worker> workers = new ArrayList<Worker>();
+
+        public Solver(float[][] matrix) throws InterruptedException {
+            N = matrix.length;
+            this.matrix = matrix;
+            barrier = new CyclicBarrier(matrix.length, new Runnable() {
+                public void run() {
+                    isDone = true;
+                    System.out.println("执行 barrier action");
+                    float sum = 0;
+                    for(Worker worker : workers){
+                        sum += worker.result;
+                    }
+                    System.out.println(String.format("计算矩阵的结果为:%f",sum));
+                }
+            });
+
+            List<Thread> threads = new ArrayList<Thread>(N);
+            for (int i = 0; i < N; i++) {
+                Worker worker = new Worker(matrix[i]);
+                workers.add(worker);
+                Thread thread = new Thread(worker);
+                threads.add(thread);
+                thread.start();
+            }
+
+            for (Thread thread : threads) {
+                thread.join();
+            }
+        }
+
+
+        class Worker implements Runnable {
+            float[] myRow;
+            float result;
+            public Worker(float[] myRow) {
+                this.myRow = myRow;
+            }
+
+            public void run() {
+                while (!done()) {
+                    processRow(myRow);
+                    try {
+                    /*
+                     * 等待其他线程全部调用完成await方法,到达barrier点之后才继续执行
+                     * 如果当前线程不是最后一个执行await方法的线程,那么它将处于等待状态
+                     * 如果当前线程是最后一个到达barrier的，非空的barrier action将又当前线程所执行
+                     * 然后其他等待线程再继续执行(通过分析await源码可以看到)
+                     * 因此barrier action的执行早于线程await方法的返回(happen before)
+                     * await返回到达barrier的索引数
+                     */
+                        int n = barrier.await();
+                        System.out.println(String.format("第%d个到达barrier", n));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (BrokenBarrierException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            private void processRow(float[] myRow) {
+                for (float num : myRow) {
+                    result += num;
+                }
+            }
+        }
+
+    }
+
+
 }
