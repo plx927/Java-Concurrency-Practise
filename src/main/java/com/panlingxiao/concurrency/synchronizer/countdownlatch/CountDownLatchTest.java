64e62cbddd52d237c23d33ca8d75e0666d2397e7
panlingxiao <784580872@qq.com>
2018-07-05 23:48:27 +0800

    CountdownLatch vs Synchronized

--- src/main/java/com/panlingxiao/concurrency/synchronizer/countdownlatch/CountDownLatchTest.java
+++ src/main/java/com/panlingxiao/concurrency/synchronizer/countdownlatch/CountDownLatchTest.java
@@ -1,6 +1,7 @@
 package com.panlingxiao.concurrency.synchronizer.countdownlatch;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by panlingxiao on 2016/6/17.
@@ -8,9 +9,9 @@ import java.util.concurrent.CountDownLatch;
  */
 public class CountDownLatchTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         final CountDownLatch countDownLatch = new CountDownLatch(1);
-        new Thread("A线程"){
+        new Thread("A线程") {
             @Override
             public void run() {
                 try {
@@ -22,7 +23,8 @@ public class CountDownLatchTest {
             }
         }.start();
 
-        new Thread("B线程"){
+        TimeUnit.SECONDS.sleep(1);
+        new Thread("B线程") {
             @Override
             public void run() {
                 System.out.println("B线程执行");
